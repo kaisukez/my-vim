a5cc515209b221190c9001f0ed689d8cd03d981d
kaisukez <zykigix@hotmail.com>
2017-10-03 21:43:59 +0700

    add myvim-install.sh

---
+++ myvim-install.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/VundleVim/Vundle.vim.git tempFolder123HAHAEIEI
+mv tempFolder123HAHAEIEI ~/.vim/bundle/Vundle.vim
+rm -rf tempFolder123HAHAEIEI
+
+git clone https://github.com/powerline/fonts.git tempFolder456HAHAEIEI --depth=1
+sh tempFolder456HAHAEIEI/install.sh
+rm -rf fonts
